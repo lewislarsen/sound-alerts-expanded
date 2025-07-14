63d7c0e5a2f4692006dea29175925914faa463c9
Lewis Larsen <lewis@larsens.dev>
2025-07-14 05:23:56 +0100

    fix: improve message string type

--- src/main/java/com/soundalertsexpanded/triggers/ChambersOfXericCompletion.java
+++ src/main/java/com/soundalertsexpanded/triggers/ChambersOfXericCompletion.java
@@ -5,6 +5,7 @@ import com.soundalertsexpanded.player.LoggedInState;
 import com.soundalertsexpanded.sound.Sound;
 import com.soundalertsexpanded.sound.SoundEngine;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.client.eventbus.Subscribe;
@@ -35,7 +36,8 @@ public class ChambersOfXericCompletion {
 
     @Subscribe
     public void onChatMessage(ChatMessage chatMessage) {
-        if (chatMessage.getMessage().contains(RAID_COMPLETION_MESSAGE)) {
+        if (chatMessage.getType() == ChatMessageType.GAMEMESSAGE &&
+                chatMessage.getMessage().contains(RAID_COMPLETION_MESSAGE)) {
             notifyRaidCompletion();
         }
     }
